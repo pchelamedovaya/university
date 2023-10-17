580a4f327bbabfaa1eee9ac22fb66e0047c6aea2
Regina <tuturiw@mail.ru>
2023-10-17 16:43:00 +0300

    for account

--- src/main/java/com/example/university/servlets/OfficeServlet.java
+++ src/main/java/com/example/university/servlets/OfficeServlet.java
@@ -1,13 +1,28 @@
 package com.example.university.servlets;
 
+import com.example.university.dao.NewsDAO;
+import com.example.university.dao.UserDAO;
+import com.example.university.entity.News;
+import com.example.university.entity.User;
+import com.example.university.utils.ConfigSingleton;
+import freemarker.template.Template;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class OfficeServlet extends HttpServlet {
+
+    public void init() {
+        ConfigSingleton.setServletContext(this.getServletContext());
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().println("OFFICE");
+        response.setContentType("text/html;charset=UTF-8");
+        response.getWriter().println("ACCOUNT");
     }
 
     @Override
---
+++ src/main/webapp/WEB-INF/view/account/office.ftl
@@ -0,0 +1,14 @@
+<#import "../macros.ftl" as macros/>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Account</title>
+    <meta charset="UTF-8">
+    <link type="text/css" rel="stylesheet" href="/university_war_exploded/styles/office.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+<@macros.navbar/>
+
+</body>
+</html>
